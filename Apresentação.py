f3d34824c27f941ac851c9d3d5cf021fc3f0c652
Milo <perrut@proton.me>
2025-02-10 20:26:34 -0300

    Apresentação
    Um Pythonzinho aí

---
+++ Apresentação.py
@@ -0,0 +1,17 @@
+print("Olá mundo, sou Vitor")
+print("Tudo bom com si?")
+print("Espero que sim? Né")
+
+nome = "Vitor"
+seguidores = "mais de 15 mil seguidores"
+
+print(f"Meu nome é {nome} e eu tenho {seguidores}!")
+
+outro_nome = input("Digite o seu nome: ")
+print(f"Olá {outro_nome}")
+
+usuario = input("Digite o seu usuário: ")
+if usuario == "Vitor":
+    print("Acesso concedido!")
+else:
+    print("Não é autorizado..")
